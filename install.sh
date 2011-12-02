a94c2ddc550c87db92e40356aa53c5e3247faf02
Gilles FELIX <felix@novius.fr>
2011-12-02 10:02:23 +0100

    Add comments in install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,15 @@ cd static
 ln -s ../../cms/static cms
 cd ../../
 
+# Possible root permission for this !!
 chmod 777 local/config
 
 mkdir local/data
 mkdir local/data/config
 echo "<?php
 
-return array();" > local/data/config/app_installed.php 
+return array();" > local/data/config/app_installed.php
 
+#chmod 777 local/data/
+
+#mkdir public/static/modules
