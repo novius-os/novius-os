36b11fdc92e6091776618fd581608f21771d7d9e
esperat <esperat@novius.fr>
2012-01-18 18:46:32 +0100

    Notify the user a long period of inactivity and ask him to login again.

--- cms/framework/classes/fuel/config.php
+++ cms/framework/classes/fuel/config.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * NOVIUS OS - Web OS for digital communication
- * 
+ *
  * @copyright  2011 Novius
  * @license    GNU Affero General Public License v3 or (at your option) any later version
  *             http://www.gnu.org/licenses/agpl-3.0.html
@@ -18,18 +18,19 @@ class Config extends \Fuel\Core\Config {
     public static function mergeWithUser($item, $config) {
         $user = Session::user();
 
+		if (!empty($user)) {
+			$item = str_replace('::', '/', $item);
+			$item = explode('/', $item);
 
+			array_splice($item, 1, 0, 'config');
 
-        $item = str_replace('::', '/', $item);
-        $item = explode('/', $item);
+			$item = implode('.', $item);
 
-        array_splice($item, 1, 0, 'config');
+			//Arr::set($config, 'configuration_id', $item);
 
-        $item = implode('.', $item);
-
-        //Arr::set($config, 'configuration_id', $item);
-
-        return \Arr::merge($config, \Arr::get($user->getConfiguration(), $item, array()));
+			$config = \Arr::merge($config, \Arr::get($user->getConfiguration(), $item, array()));
+		}
+		return $config;
     }
 
 }
