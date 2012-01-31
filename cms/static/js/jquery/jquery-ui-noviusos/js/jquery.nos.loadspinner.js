4577595c001e77c68851f9f5111d90e0438d78e6
Sébastien Drouyer <drouyer@novius.com>
2012-01-31 14:31:38 +0100

    Fixing loadspinner error...

--- cms/static/js/jquery/jquery-ui-noviusos/js/jquery.nos.loadspinner.js
+++ cms/static/js/jquery/jquery-ui-noviusos/js/jquery.nos.loadspinner.js
@@ -1,6 +1,6 @@
 /**
  * NOVIUS OS - Web OS for digital communication
- * 
+ *
  * @copyright  2011 Novius
  * @license    GNU Affero General Public License v3 or (at your option) any later version
  *             http://www.gnu.org/licenses/agpl-3.0.html
@@ -347,7 +347,7 @@ define([
 			if (!this.running) {
 				this.running = true;
 				var t = this;
-				this.timer = self.setInterval(function () {
+				this.timer = window.setInterval(function () {
 					t._tick();
 				}, Math.round(1000 / this.options.fps));
 			}
