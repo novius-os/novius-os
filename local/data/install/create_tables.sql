2bcfdd5338e3a6f3b33d820911f9bf8aec4aadca
Julian Esperat <esperat@novius.fr>
2012-02-02 10:58:11 +0100

    Updating create_tables.sql

--- local/data/install/create_tables.sql
+++ local/data/install/create_tables.sql
@@ -7,182 +7,83 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
--- --------------------------------------------------------
---
--- Structure de la table `cms_blog_categorie`
---
-
-CREATE TABLE IF NOT EXISTS `cms_blog_categorie` (
-  `blgc_id` int(11) NOT NULL auto_increment,
-  `blgc_parent_id` int(11) default NULL,
-  `blgc_titre` varchar(255) collate latin1_general_ci NOT NULL default '',
-  `blgc_niveau` tinyint(4) NOT NULL default '0',
-  `blgc_rail` varchar(255) collate latin1_general_ci default NULL,
-  `blgc_rang` float default NULL,
-  PRIMARY KEY  (`blgc_id`),
-  KEY `blgc_parent_id` (`blgc_parent_id`),
-  KEY `blgc_rail` (`blgc_rail`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
-
-
--- --------------------------------------------------------
---
--- Structure de la table `cms_blog_lien_categorie`
---
-
-CREATE TABLE IF NOT EXISTS `cms_blog_lien_categorie` (
-  `blog_id` int(11) NOT NULL default '0',
-  `blgc_id` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`blog_id`,`blgc_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-
-
--- --------------------------------------------------------
---
--- Structure de la table `cms_blog_tag`
---
-
-CREATE TABLE IF NOT EXISTS `cms_blog_tag` (
-  `blgt_blog_id` int(10) unsigned NOT NULL default '0',
-  `blgt_tag_id` int(11) NOT NULL,
-  PRIMARY KEY  (`blgt_blog_id`,`blgt_tag_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-
-
--- --------------------------------------------------------
---
--- Structure de la table `cms_commentaire`
---
-
-CREATE TABLE IF NOT EXISTS `cms_commentaire` (
-  `comm_id` int(10) unsigned NOT NULL auto_increment,
-  `comm_type` varchar(50) collate latin1_general_ci NOT NULL default '',
-  `comm_parent_id` int(10) unsigned NOT NULL default '0',
-  `comm_parent_title` varchar(255) collate latin1_general_ci default NULL,
-  `comm_parent_url` text collate latin1_general_ci,
-  `comm_email` varchar(100) collate latin1_general_ci NOT NULL default '',
-  `comm_auteur` varchar(100) collate latin1_general_ci NOT NULL default '',
-  `comm_contenu` text collate latin1_general_ci NOT NULL,
-  `comm_date_creation` datetime NOT NULL default '0000-00-00 00:00:00',
-  `comm_ip` varchar(15) collate latin1_general_ci NOT NULL default '',
-  `comm_etat` enum('publier','en attente','refuser') collate latin1_general_ci NOT NULL default 'en attente',
-  `comm_blacklist` tinyint(1) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`comm_id`),
-  KEY `comm_type` (`comm_type`,`comm_parent_id`),
-  KEY `comm_etat` (`comm_etat`,`comm_blacklist`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
---
--- Structure de la table `cms_media`
---
-
-CREATE TABLE IF NOT EXISTS `cms_media` (
-  `media_id` int(10) unsigned NOT NULL auto_increment,
-  `media_path_id` int(10) unsigned NOT NULL,
-  `media_path` varchar(255) NOT NULL,
-  `media_file` varchar(100) NOT NULL,
-  `media_ext` varchar(4) NOT NULL,
-  `media_title` varchar(50) NOT NULL,
-  `media_module` varchar(30) default NULL,
-  `media_protected` tinyint(1) NOT NULL default '0',
-  `media_width` smallint(5) unsigned default NULL,
-  `media_height` smallint(5) unsigned default NULL,
-  PRIMARY KEY  (`media_id`),
-  KEY `media_path_id` (`media_path_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
---
--- Structure de la table `cms_media_folder`
---
-
-CREATE TABLE IF NOT EXISTS `cms_media_folder` (
-  `medif_id` int(10) unsigned NOT NULL auto_increment,
-  `medif_parent_id` int(10) unsigned default NULL,
-  `medif_path` varchar(255) NOT NULL,
-  `medif_title` varchar(50) NOT NULL,
-  PRIMARY KEY  (`medif_id`),
-  KEY `medip_parent_id` (`medif_parent_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
--- --------------------------------------------------------
---
--- Structure de la table `cms_page`
---
-
-CREATE TABLE IF NOT EXISTS `cms_page` (
-  `page_id` int(11) NOT NULL auto_increment,
-  `page_rac_id` char(2) collate latin1_general_ci NOT NULL default '',
-  `page_pere_id` int(11) default NULL,
-  `page_gab` varchar(255) collate latin1_general_ci NOT NULL default '',
-  `page_niveau` tinyint(4) NOT NULL default '0',
-  `page_titre` varchar(255) collate latin1_general_ci NOT NULL default '',
-  `page_titre_menu` varchar(255) collate latin1_general_ci default NULL,
-  `page_titre_reference` varchar(255) collate latin1_general_ci default NULL,
-  `page_contenu` text collate latin1_general_ci,
-  `page_contenu_text` text collate latin1_general_ci,
-  `page_travail_contenu` text collate latin1_general_ci,
-  `page_html_brut` tinyint(4) default '0',
-  `page_rang` float default NULL,
-  `page_menu` tinyint(4) NOT NULL default '0',
-  `page_type` tinyint(4) NOT NULL default '0',
-  `page_publier` tinyint(4) NOT NULL default '0',
-  `page_a_publier` datetime default NULL,
-  `page_noindex` tinyint(1) unsigned NOT NULL default '0',
-  `page_demandeur_id` int(11) default NULL,
-  `page_verrou` tinyint(4) NOT NULL default '0',
-  `page_home` tinyint(4) NOT NULL default '0',
-  `page_carrefour` tinyint(4) NOT NULL default '0',
-  `page_duree_vie` int(11) default NULL,
-  `page_nom_virtuel` varchar(50) collate latin1_general_ci default NULL,
-  `page_url_virtuel` varchar(255) collate latin1_general_ci default NULL,
-  `page_lien_externe` varchar(255) collate latin1_general_ci default NULL,
-  `page_lien_externe_type` tinyint(4) default NULL,
-  `page_date_creation` int(11) NOT NULL default '0',
-  `page_date_modif` int(11) NOT NULL default '0',
-  `page_description` text collate latin1_general_ci,
-  `page_keywords` text collate latin1_general_ci,
-  `page_addheader` text collate latin1_general_ci,
-  PRIMARY KEY  (`page_id`),
-  UNIQUE KEY `page_url_virtuel` (`page_url_virtuel`),
-  KEY `page_rac_id` (`page_rac_id`),
-  KEY `page_pere_id` (`page_pere_id`),
-  KEY `page_gab` (`page_gab`),
-  KEY `page_demandeur_id` (`page_demandeur_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci PACK_KEYS=0 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
 --
 -- Structure de la table `os_blog`
 --
 
 CREATE TABLE IF NOT EXISTS `os_blog` (
   `blog_id` int(10) unsigned NOT NULL auto_increment,
-  `blog_lang` varchar(5) collate latin1_general_ci NOT NULL,
+  `blog_lang` varchar(5) NOT NULL,
   `blog_lang_common_id` int(11) NOT NULL,
   `blog_lang_single_id` int(11) default NULL,
-  `blog_titre` varchar(255) collate latin1_general_ci NOT NULL default '',
-  `blog_auteur` varchar(255) collate latin1_general_ci default NULL,
-  `blog_auteur_id` int(10) unsigned default NULL,
-  `blog_resume` text collate latin1_general_ci,
-  `blog_contenu_wysiwyg_id` int(10) unsigned default NULL,
-  `blog_contenu_text` text collate latin1_general_ci,
-  `blog_date_creation` datetime NOT NULL default '0000-00-00 00:00:00',
-  `blog_date_debut_publication` datetime default NULL,
-  `blog_date_fin_publication` datetime default NULL,
-  `blog_lu` int(10) unsigned NOT NULL default '0',
-  `blog__source_id` int(10) unsigned default NULL,
-  `blog__titre_initial` varchar(255) collate latin1_general_ci default NULL,
-  `blog__source_txt` varchar(255) collate latin1_general_ci default NULL,
-  `blog__source_url` varchar(255) collate latin1_general_ci default NULL,
-  `blog__importance` enum('important','interessant','strategique') collate latin1_general_ci default NULL,
-  `blog__date_info` date default NULL,
-  `blog__media_thumbnail` int(10) unsigned NOT NULL,
+  `blog_title` varchar(255) NOT NULL default '',
+  `blog_author` varchar(255) default NULL,
+  `blog_author_id` int(10) unsigned default NULL,
+  `blog_summary` text,
+  `blog_created_at` datetime NOT NULL default '0000-00-00 00:00:00',
+  `blog_publication_start` datetime default NULL,
+  `blog_publication_end` datetime default NULL,
+  `blog_read` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`blog_id`),
-  KEY `blog_auteur_id` (`blog_auteur_id`),
+  KEY `blog_auteur_id` (`blog_author_id`),
   KEY `blog_lang` (`blog_lang`,`blog_lang_common_id`,`blog_lang_single_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `os_blog_category`
+--
+
+CREATE TABLE IF NOT EXISTS `os_blog_category` (
+  `blgc_id` int(11) NOT NULL auto_increment,
+  `blgc_parent_id` int(11) default NULL,
+  `blgc_title` varchar(255) NOT NULL default '',
+  `blgc_level` tinyint(4) NOT NULL default '0',
+  `blgc_path_to_category` varchar(255) default NULL,
+  `blgc_sort` float default NULL,
+  PRIMARY KEY  (`blgc_id`),
+  KEY `blgc_parent_id` (`blgc_parent_id`),
+  KEY `blgc_rail` (`blgc_path_to_category`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+-- --------------------------------------------------------
+--
+-- Structure de la table `os_blog_category_link`
+--
+
+CREATE TABLE IF NOT EXISTS `os_blog_category_link` (
+  `blog_id` int(11) NOT NULL default '0',
+  `blgc_id` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`blog_id`,`blgc_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+--
+-- Structure de la table `os_comment`
+--
+
+CREATE TABLE IF NOT EXISTS `os_comment` (
+  `comm_id` int(10) unsigned NOT NULL auto_increment,
+  `comm_type` varchar(50) NOT NULL default '',
+  `comm_parent_id` int(10) unsigned NOT NULL default '0',
+  `comm_parent_title` varchar(255) default NULL,
+  `comm_parent_url` text,
+  `comm_email` varchar(100) NOT NULL default '',
+  `comm_author` varchar(100) NOT NULL default '',
+  `comm_content` text NOT NULL,
+  `comm_created_at` datetime NOT NULL default '0000-00-00 00:00:00',
+  `comm_ip` varchar(15) NOT NULL default '',
+  `comm_state` enum('published','pending','refused') NOT NULL default 'pending',
+  `comm_blacklist` tinyint(1) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`comm_id`),
+  KEY `comm_type` (`comm_type`,`comm_parent_id`),
+  KEY `comm_etat` (`comm_state`,`comm_blacklist`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
 
 -- --------------------------------------------------------
 --
@@ -195,6 +96,7 @@ CREATE TABLE IF NOT EXISTS `os_group` (
   `group_user_id` int(10) unsigned NOT NULL,
   PRIMARY KEY  (`group_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
 
 -- --------------------------------------------------------
 --
@@ -209,6 +111,136 @@ CREATE TABLE IF NOT EXISTS `os_group_permission` (
   UNIQUE KEY `perm_group_id` (`perm_group_id`,`perm_module`,`perm_key`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+
+-- --------------------------------------------------------
+--
+-- Structure de la table `os_media`
+--
+
+CREATE TABLE IF NOT EXISTS `os_media` (
+  `media_id` int(10) unsigned NOT NULL auto_increment,
+  `media_path_id` int(10) unsigned NOT NULL,
+  `media_path` varchar(255) NOT NULL,
+  `media_file` varchar(100) NOT NULL,
+  `media_ext` varchar(4) NOT NULL,
+  `media_title` varchar(50) NOT NULL,
+  `media_module` varchar(30) default NULL,
+  `media_protected` tinyint(1) NOT NULL default '0',
+  `media_width` smallint(5) unsigned default NULL,
+  `media_height` smallint(5) unsigned default NULL,
+  PRIMARY KEY  (`media_id`),
+  KEY `media_path_id` (`media_path_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+-- --------------------------------------------------------
+--
+-- Structure de la table `os_media_folder`
+--
+
+CREATE TABLE IF NOT EXISTS `os_media_folder` (
+  `medif_id` int(10) unsigned NOT NULL auto_increment,
+  `medif_parent_id` int(10) unsigned default NULL,
+  `medif_path` varchar(255) NOT NULL,
+  `medif_title` varchar(50) NOT NULL,
+  PRIMARY KEY  (`medif_id`),
+  KEY `medip_parent_id` (`medif_parent_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+REPLACE INTO `os_media_folder` (`medif_id`, `medif_parent_id`, `medif_path`, `medif_title`) VALUES
+(1, NULL, '', 'Media library');
+
+
+-- --------------------------------------------------------
+--
+-- Structure de la table `os_media_link`
+--
+
+CREATE TABLE IF NOT EXISTS `os_media_link` (
+  `medil_id` int(10) unsigned NOT NULL auto_increment,
+  `medil_from_table` varchar(255) NOT NULL,
+  `medil_foreign_id` int(10) unsigned NOT NULL,
+  `medil_key` varchar(30) NOT NULL,
+  `medil_media_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`medil_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+-- --------------------------------------------------------
+--
+-- Structure de la table `os_page`
+--
+
+CREATE TABLE IF NOT EXISTS `os_page` (
+  `page_id` int(11) NOT NULL auto_increment,
+  `page_root_id` char(2) NOT NULL default '',
+  `page_parent_id` int(11) default NULL,
+  `page_template` varchar(255) NOT NULL,
+  `page_level` tinyint(4) NOT NULL default '0',
+  `page_title` varchar(255) NOT NULL default '',
+  `page_menu_title` varchar(255) default NULL,
+  `page_meta_title` varchar(255) default NULL,
+  `page_search_words` text,
+  `page_raw_html` tinyint(4) default '0',
+  `page_sort` float default NULL,
+  `page_menu` tinyint(4) NOT NULL default '0',
+  `page_type` tinyint(4) NOT NULL default '0',
+  `page_published` tinyint(4) NOT NULL default '0',
+  `page_publication_start` datetime default NULL,
+  `page_meta_noindex` tinyint(1) unsigned NOT NULL default '0',
+  `page_requested_by_user_id` int(11) default NULL,
+  `page_lock` tinyint(4) NOT NULL default '0',
+  `page_entrance` tinyint(4) NOT NULL default '0',
+  `page_home` tinyint(4) NOT NULL default '0',
+  `page_cache_duration` int(11) default NULL,
+  `page_virtual_name` varchar(50) default NULL,
+  `page_virtual_url` varchar(255) default NULL,
+  `page_external_link` varchar(255) default NULL,
+  `page_external_link_type` tinyint(4) default NULL,
+  `page_created_at` int(11) NOT NULL default '0',
+  `page_updated_at` int(11) NOT NULL default '0',
+  `page_meta_description` text,
+  `page_meta_keywords` text,
+  `page_head_additional` text,
+  PRIMARY KEY  (`page_id`),
+  UNIQUE KEY `page_url_virtuel` (`page_virtual_url`),
+  KEY `page_rac_id` (`page_root_id`),
+  KEY `page_pere_id` (`page_parent_id`),
+  KEY `page_demandeur_id` (`page_requested_by_user_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+-- --------------------------------------------------------
+--
+-- Structure de la table `os_page_root`
+--
+
+CREATE TABLE IF NOT EXISTS `os_page_root` (
+  `rac_id` char(2) NOT NULL default '',
+  `rac_title` varchar(30) NOT NULL default '',
+  `rac_gab_id` int(11) NOT NULL default '0',
+  `rac_gab_popup_id` int(11) default NULL,
+  `rac_mobile` tinyint(1) unsigned NOT NULL,
+  `rac_sort` tinyint(4) default NULL,
+  PRIMARY KEY  (`rac_id`),
+  KEY `rac_gab_id` (`rac_gab_id`),
+  KEY `rac_gab_popup_id` (`rac_gab_popup_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+--
+-- Structure de la table `os_tag`
+--
+
+CREATE TABLE IF NOT EXISTS `os_tag` (
+  `tag_id` int(11) NOT NULL auto_increment,
+  `tag_label` varchar(255) NOT NULL,
+  PRIMARY KEY  (`tag_id`),
+  UNIQUE KEY `tag_label` (`tag_label`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
 -- --------------------------------------------------------
 --
 -- Structure de la table `os_user`
@@ -221,8 +253,9 @@ CREATE TABLE IF NOT EXISTS `os_user` (
   `user_password` varchar(64) NOT NULL,
   `user_last_connection` datetime NOT NULL,
   `user_configuration` text,
-  PRIMARY KEY (`user_id`)
+  PRIMARY KEY  (`user_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
 
 -- --------------------------------------------------------
 --
@@ -233,6 +266,7 @@ CREATE TABLE IF NOT EXISTS `os_user_group` (
   `user_id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 
 -- --------------------------------------------------------
 --
@@ -245,17 +279,6 @@ CREATE TABLE IF NOT EXISTS `os_wysiwyg` (
   `wysiwyg_join_table` varchar(255) NOT NULL,
   `wysiwyg_key` varchar(30) NOT NULL,
   `wysiwyg_foreign_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`wysiwyg_id`)
+  PRIMARY KEY  (`wysiwyg_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-
---
--- Contenu de la table `cms_media_folder`
---
-
-REPLACE INTO `cms_media_folder` (`medif_id`, `medif_parent_id`, `medif_path`, `medif_title`) VALUES
-(1, NULL, '', 'Media library');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
