d68be613f00c85b9a18ce7689f6bdd0359227b38
Sébastien Drouyer <drouyer@novius.com>
2012-02-10 10:40:14 +0100

    Adding warning...

--- cms/framework/classes/fuel/controller.php
+++ cms/framework/classes/fuel/controller.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright  2011 Novius
+ * @license    GNU Affero General Public License v3 or (at your option) any later version
+ *             http://www.gnu.org/licenses/agpl-3.0.html
+ * @link http://www.novius-os.org
+ */
+
+
+// Warning : functions might be localised on other an other controller
+// (like Controller_Configurable) in near future...
 class Controller extends Fuel\Core\Controller {
     protected $config = array();
 
