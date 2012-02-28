badbf3d604bb51ecee87e564151ca0371644d5d5
Gilles FELIX <felix@novius.com>
2012-02-28 15:05:28 +0100

    Template blog : remove call of a old js file

--- local/modules/tpl_default/templates/blog.php
+++ local/modules/tpl_default/templates/blog.php
@@ -1,8 +1,3 @@
-<?= PHP_BEGIN ?>
-    //if (in_array(PAGE_CONTRIBUTIONS, $GLOBALS['page_rail'])) {
-    //    $GLOBALS['langue'] = 'uk';
-    //}
-<?= PHP_END ?>
 <?php
     //if (in_array(PAGE_CONTRIBUTIONS, (array) $GLOBALS['page_rail'])) {
     //    $GLOBALS['langue'] = 'uk';
@@ -19,7 +14,6 @@
   <link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
   <link rel="stylesheet" type="text/css" href="static/css/blog.css">
   <link rel="stylesheet" href="static/js/orangebox/css/orangebox.css" type="text/css" />
-  <script src="cms/fonctions.js" type="text/javascript"></script>
   <script type="text/javascript" src="static/js/jquery.js"></script>
   <script type="text/javascript" src="static/js/orangebox/js/orangebox.min.js"></script>
   <script type="text/javascript" src="static/js/site.js"></script>
