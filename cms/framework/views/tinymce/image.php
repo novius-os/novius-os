562fc82531db7b3c19b961f269d556d58a558a0c
Gilles FELIX <felix@novius.com>
2012-03-16 14:22:42 +0100

    Add comment licence

--- cms/framework/views/tinymce/image.php
+++ cms/framework/views/tinymce/image.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright  2011 Novius
+ * @license    GNU Affero General Public License v3 or (at your option) any later version
+ *             http://www.gnu.org/licenses/agpl-3.0.html
+ * @link http://www.novius-os.org
+ */
+?>
+<?php
 	$mp3view = (string) Request::forge('admin/media/list/index')->execute(array('image_pick'))->response();
 ?>
 <div id="<?= $uniqid = uniqid('tabs_') ?>">
