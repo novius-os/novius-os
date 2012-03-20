41cc6284e3381cff486dbcbd01e66f763ee4d69b
esperat <esperat@novius.fr>
2012-03-20 11:38:28 +0100

    Updating demo.

--- public/static/js/site.js
+++ public/static/js/site.js
@@ -1,21 +1,31 @@
-/**
- * NOVIUS OS - Web OS for digital communication
- * 
- * @copyright  2011 Novius
- * @license    GNU Affero General Public License v3 or (at your option) any later version
- *             http://www.gnu.org/licenses/agpl-3.0.html
- * @link http://www.novius-os.org
- */
-$(document).ready(function() {
-
-    $('a[href$="reso.htm"]').each(function() {
-        $(this).attr('href', $(this).attr('href') + '?iframe').orangeBox({
-            iframeWidth : 1040,
-            iframeHeight : 631,
-            addThis : false
-        });
-        if (window.location.hash == '#play') {
-            $(this).click();
-        }
+// initialise menu
+jQuery(document).ready(function() {
+    jQuery('#header ul').superfish();
+    jQuery('#header_home ul').superfish();
+    jQuery('#sidebar ul').superfish({
+        'animation' : {height : 'show'},   // slide-down effect without fade-in
+        delay       : 500,
+        autoArrows  : true
     });
+    auto_height_content();
 });
+
+function auto_height_content(){
+	var  screenH = 0;
+	if( typeof( window.innerWidth ) == 'number' ) {
+        //Non-IE
+        screenH = window.innerHeight;
+    } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+        //IE 6+ in 'standards compliant mode'
+        screenH = document.documentElement.clientHeight;
+    } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+        //IE 300 compatible
+        screenH = document.body.clientHeight;
+    }
+	var $height_content = $('#container').height()+$('#footer').height();
+	if($height_content < screenH)
+	{
+		var min_height = screenH - $('#footer').height();
+		$('#container').css({minHeight:min_height+'px'});
+	}
+}
