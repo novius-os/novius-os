8cdc53a8d5ed772c735b6c550188af72fddfd74d
Gilles FELIX <felix@novius.com>
2012-08-17 17:13:59 +0200

    Add observer created and updated at on models

--- local/migrations/006_created_and_updated_at.php
+++ local/migrations/006_created_and_updated_at.php
@@ -2,16 +2,28 @@
 
 namespace Fuel\Migrations;
 
-class Remove_page_root
+class Created_and_updated_at
 {
     public function up()
     {
-        \DB::query("ALTER TABLE `nos_media_folder` ADD `medif_dir_name` VARCHAR( 50 ) NULL AFTER `medif_path`;")->execute();
-        \DB::query("UPDATE `nos_media_folder` SET `medif_dir_name` = REPLACE(SUBSTRING_INDEX(`medif_path`, '/', -2), '/', '');")->execute();
-        \DB::query("UPDATE `nos_media` SET `media_file` = REPLACE(`media_file`, CONCAT('.', `media_ext`), '');")->execute();
-        \DB::query("UPDATE `nos_media` SET `media_path` = CONCAT(`media_path`, `media_file`, '.', `media_ext`);")->execute();
-   }
+        \DB::query("ALTER TABLE `nos_blog_category` ADD `cat_created_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+          ADD `cat_updated_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00';")->execute();
 
+        \DB::query("ALTER TABLE `nos_news_category` ADD `cat_created_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+          ADD `cat_updated_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00';")->execute();
+
+        \DB::query("ALTER TABLE `nos_page` CHANGE `page_created_at` `page_created_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+          CHANGE `page_updated_at` `page_updated_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00';")->execute();
+
+        \DB::query("ALTER TABLE `nos_user` ADD `user_created_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+          ADD `user_updated_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00';")->execute();
+
+        \DB::query("ALTER TABLE `nos_media` ADD `media_created_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+          ADD `media_updated_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00';")->execute();
+
+        \DB::query("ALTER TABLE `nos_media_folder` ADD `medif_created_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+          ADD `medif_updated_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00';")->execute();
+    }
     public function down()
     {
 
