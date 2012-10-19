df678c527c9d480153fc95f526130982f7357995
Gilles FELIX <felix@novius.com>
2012-10-19 09:38:00 +0200

    Migration 0.2 : add an update of url_enhanced config file, integrate contexts

--- local/migrations/003_version_0_2.php
+++ local/migrations/003_version_0_2.php
@@ -7,6 +7,7 @@ class Version_0_2
     public function up()
     {
         // Rename lang, lang_common_id, lan_is_main columns. Replace lang by context. Resize lang columns.
+        // Update context's columns with site::locale
         $alters = <<<SQL
 ALTER TABLE `nos_blog_category` CHANGE `cat_lang` `cat_context` VARCHAR( 25 ) NOT NULL, CHANGE `cat_lang_common_id` `cat_context_common_id` INT( 11 ) NOT NULL, CHANGE `cat_lang_is_main` `cat_context_is_main` TINYINT( 1 ) NOT NULL DEFAULT '0';
 ALTER TABLE `nos_blog_post` CHANGE `post_lang` `post_context` VARCHAR( 25 ) NOT NULL, CHANGE `post_lang_common_id` `post_context_common_id` INT( 11 ) NOT NULL, CHANGE `post_lang_is_main` `post_context_is_main` TINYINT( 1 ) NOT NULL DEFAULT '0';
@@ -27,7 +28,25 @@ SQL;
             }
         }
 
+        // Clear pages cache, now cache use domain
         \File::delete_dir(\Config::get('cache_dir').'pages', true, false);
+
+
+        // Update url_enhanced config file, integrate contexts
+        \Config::load(APPPATH.'data'.DS.'config'.DS.'url_enhanced.php', 'data::url_enhanced');
+
+        $url_enhanced_old = \Config::get("data::url_enhanced", array());
+        $url_enhanced_new = array();
+        foreach ($url_enhanced_old as $page_id) {
+            $page = \Nos\Model_Page::find($page_id);
+            if (!empty($page)) {
+                $url_enhanced_new[$page_id] = array(
+                    'url' => $page->page_entrance ? '' : $page->virtual_path(true),
+                    'context' => $page->page_context,
+                );
+            }
+        }
+        \Config::save(APPPATH.'data'.DS.'config'.DS.'url_enhanced.php', $url_enhanced_new);
     }
 
     public function down()
