8ff7a74378e0dcb8b34872a62ffb9ca79e61a1b3
Gilles FELIX <felix@novius.com>
2013-03-05 09:36:20 +0100

    Code sniffer

--- local/migrations/004_move_migration_table.php
+++ local/migrations/004_move_migration_table.php
@@ -7,7 +7,7 @@ class Move_migration_table
     public function up()
     {
         \Config::load('migrations', true);
-		$table = \Config::get('migrations.table');
+        $table = \Config::get('migrations.table');
         if ($table == 'migration') {
             \DB::query('RENAME TABLE `novius_os`.`migration` TO  `novius_os`.`nos_migration`;')->execute();
             \Migrate::set_table('nos_migration');
