a36c7ff5207c298cebae143c1ae80430586e7a51
Julian Esperat <esperat@novius.fr>
2013-03-05 17:42:32 +0100

    Bugfix migration: removed the DB name

--- local/migrations/004_move_migration_table.php
+++ local/migrations/004_move_migration_table.php
@@ -9,7 +9,7 @@ class Move_migration_table
         \Config::load('migrations', true);
         $table = \Config::get('migrations.table');
         if ($table == 'migration') {
-            \DB::query('RENAME TABLE `novius_os`.`migration` TO  `novius_os`.`nos_migration`;')->execute();
+            \DB::query('RENAME TABLE `migration` TO  `nos_migration`;')->execute();
             \Migrate::set_table('nos_migration');
             \Config::set('migrations.table', 'nos_migration');
         }
